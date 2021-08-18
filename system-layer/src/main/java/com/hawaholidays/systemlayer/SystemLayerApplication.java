fc712decfbd2eaa1469ee84548e1679a8e7ee97f
rhish9h <rhish9h@gmail.com>
2021-08-18 11:58:17 +0530

    init commit

---
+++ system-layer/src/main/java/com/hawaholidays/systemlayer/SystemLayerApplication.java
@@ -0,0 +1,13 @@
+package com.hawaholidays.systemlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemLayerApplication.class, args);
+	}
+
+}
---
+++ system-layer/src/test/java/com/hawaholidays/systemlayer/SystemLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawaholidays.systemlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
