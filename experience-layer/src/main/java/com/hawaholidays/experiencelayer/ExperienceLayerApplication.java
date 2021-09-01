0294b56bfce3d1cf9ac848592b65137c5b2f285d
rhish9h <rhish9h@gmail.com>
2021-09-01 17:06:49 +0530

    added experience layer basic project

---
+++ experience-layer/src/main/java/com/hawaholidays/experiencelayer/ExperienceLayerApplication.java
@@ -0,0 +1,13 @@
+package com.hawaholidays.experiencelayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperienceLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperienceLayerApplication.class, args);
+	}
+
+}
---
+++ experience-layer/src/test/java/com/hawaholidays/experiencelayer/ExperienceLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawaholidays.experiencelayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperienceLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
